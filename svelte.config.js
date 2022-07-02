79f030b5cf81601323bd1d830c1c12c9c9d72bbb
jjelliott <jesseelliott@outlook.com>
2022-07-02 03:05:17 -0400

    Try again

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import {sveltePreprocess} from "svelte-preprocess/dist/autoProcess.js";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: {default: true},
 		adapter: adapter({
 			pages:"docs",
 			assets:"docs"
